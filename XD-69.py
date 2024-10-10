d9fd33ede0bbf47e316873a7ce892119d54ccd8a
ESHAN-69 <nr0084684@gmail.com>
2024-10-10 09:18:12 +0600

    Update XD-69.py

--- XD-69.py
+++ XD-69.py
@@ -1,8 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/python
-import platform,os
-os.system("git pull")
+import os,sys,platform
 bit = platform.architecture()[0]
-if bit == '64bit':
-    import eshan_64
-elif bit == '32bit':
-    import eshan_32
+if bit == '64bit':os.system('chmod 777 eshan_64;./eshan_64')
+elif bit == '32bit':os.system('chmod 777 eshan_32;./eshan_32)
