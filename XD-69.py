5ec4aa7104d399ebc8c0a1894cbdc0b1ea1ba352
ESHAN-69 <nr0084684@gmail.com>
2024-10-10 08:14:09 +0600

    Update XD-69.py

--- XD-69.py
+++ XD-69.py
@@ -1,2 +1,8 @@
-try:import eshan_64
-except:import eshan_32
+#!/data/data/com.termux/files/usr/bin/python
+import platform,os
+os.system("git pull")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import eshan_64
+elif bit == '32bit':
+    import eshan_32
